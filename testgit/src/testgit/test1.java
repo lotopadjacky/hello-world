efa0f0510bfda3335f341e4be13dc76fc7450d0f
lotopad <1669633326@qq.com>
2023-08-05 20:54:02 +0800

    4.test1.class代码修改

--- testgit/src/testgit/test1.java
+++ testgit/src/testgit/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("测试 git");
+   System.out.println("测试222222 git");
 	}
 
 }
